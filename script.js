a760ebfdaa8cba854a66f6a203f5829051504f93
Cesar Soltero Perez <64189944+Cesarsp41@users.noreply.github.com>
2024-11-27 22:55:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,463 +21,80 @@ let ironFields = [0, 0, 0, 0];
 let cerealFields = [0, 0, 0, 0, 0];
 
 // Precios de casillas
+const resourcesPrices = [];
+
+
 const woodPrices = [
-    {
-        level: 1,
-        wood: 40,
-        clay: 100,
-        iron: 50,
-        cereal: 60
-    },
-    {
-        level: 2,
-        wood: 65,
-        clay: 165,
-        iron: 85,
-        cereal: 100
-    },
-    {
-        level: 3,
-        wood: 110,
-        clay: 280,
-        iron: 140,
-        cereal: 165
-    },
-    {
-        level: 4,
-        wood: 185,
-        clay: 465,
-        iron: 235,
-        cereal: 280
-    },
-    {
-        level: 5,
-        wood: 310,
-        clay: 780,
-        iron: 390,
-        cereal: 465
-    },
-    {
-        level: 6,
-        wood: 520,
-        clay: 1300,
-        iron: 650,
-        cereal: 780
-    },
-    {
-        level: 7,
-        wood: 870,
-        clay: 2170,
-        iron: 1085,
-        cereal: 1300
-    },
-    {
-        level: 8,
-        wood: 1450,
-        clay: 3625,
-        iron: 1810,
-        cereal: 2175
-    },
-    {
-        level: 9,
-        wood: 2420,
-        clay: 6050,
-        iron: 3025,
-        cereal: 3630
-    },
-    {
-        level: 10,
-        wood: 4040,
-        clay: 10105,
-        iron: 5050,
-        cereal: 6060
-    }
-];
+    {level: 1, wood: 40, clay: 100, iron: 50, cereal: 60},
+    {level: 2, wood: 65, clay: 165, iron: 85, cereal: 100},
+    {level: 3, wood: 110, clay: 280, iron: 140, cereal: 165},
+    {level: 4, wood: 185, clay: 465, iron: 235, cereal: 280},
+    {level: 5, wood: 310, clay: 780, iron: 390, cereal: 465},
+    {level: 6, wood: 520, clay: 1300, iron: 650, cereal: 780},
+    {level: 7, wood: 870, clay: 2170, iron: 1085, cereal: 1300},
+    {level: 8, wood: 1450, clay: 3625, iron: 1810, cereal: 2175},
+    {level: 9, wood: 2420, clay: 6050, iron: 3025, cereal: 3630},
+    {level: 10, wood: 4040, clay: 10105, iron: 5050, cereal: 6060}];
 
 const clayPrices = [
-    {
-        level: 1,
-        wood: 80,
-        clay: 40,
-        iron: 80,
-        cereal: 50
-    },
-    {
-        level: 2,
-        wood: 135,
-        clay: 65,
-        iron: 135,
-        cereal: 85
-    },
-    {
-        level: 3,
-        wood: 225,
-        clay: 110,
-        iron: 225,
-        cereal: 140
-    },
-    {
-        level: 4,
-        wood: 375,
-        clay: 185,
-        iron: 375,
-        cereal: 235
-    },
-    {
-        level: 5,
-        wood: 620,
-        clay: 310,
-        iron: 620,
-        cereal: 390
-    },
-    {
-        level: 6,
-        wood: 1040,
-        clay: 520,
-        iron: 1040,
-        cereal: 650
-    },
-    {
-        level: 7,
-        wood: 1735,
-        clay: 870,
-        iron: 1735,
-        cereal: 1085
-    },
-    {
-        level: 8,
-        wood: 2900,
-        clay: 1450,
-        iron: 2900,
-        cereal: 1810
-    },
-    {
-        level: 9,
-        wood: 4840,
-        clay: 2420,
-        iron: 4840,
-        cereal: 3025
-    },
-    {
-        level: 10,
-        wood: 8080,
-        clay: 4040,
-        iron: 8080,
-        cereal: 5050
-    }
-];
+    {level: 1,wood: 80,clay: 40,iron: 80,cereal: 50},
+    {level: 2,wood: 135,clay: 65,iron: 135,cereal: 85},
+    {level: 3,wood: 225,clay: 110,iron: 225,cereal: 140},
+    {level: 4,wood: 375,clay: 185,iron: 375,cereal: 235},
+    {level: 5,wood: 620,clay: 310,iron: 620,cereal: 390},
+    {level: 6,wood: 1040,clay: 520,iron: 1040,cereal: 650},
+    {level: 7,wood: 1735,clay: 870,iron: 1735,cereal: 1085},
+    {level: 8,wood: 2900,clay: 1450,iron: 2900,cereal: 1810},
+    {level: 9,wood: 4840,clay: 2420,iron: 4840,cereal: 3025},
+    {level: 10,wood: 8080,clay: 4040,iron: 8080,cereal: 5050}];
 
 const ironPrices = [
-    {
-        level: 1,
-        wood: 100,
-        clay: 80,
-        iron: 30,
-        cereal: 60
-    },
-    {
-        level: 2,
-        wood: 165,
-        clay: 135,
-        iron: 50,
-        cereal: 100
-    },
-    {
-        level: 3,
-        wood: 280,
-        clay: 225,
-        iron: 85,
-        cereal: 165
-    },
-    {
-        level: 4,
-        wood: 465,
-        clay: 375,
-        iron: 140,
-        cereal: 280
-    },
-    {
-        level: 5,
-        wood: 780,
-        clay: 620,
-        iron: 235,
-        cereal: 465
-    },
-    {
-        level: 6,
-        wood: 1300,
-        clay: 1040,
-        iron: 390,
-        cereal: 780
-    },
-    {
-        level: 7,
-        wood: 2170,
-        clay: 1735,
-        iron: 650,
-        cereal: 1300
-    },
-    {
-        level: 8,
-        wood: 3625,
-        clay: 2900,
-        iron: 1085,
-        cereal: 2175
-    },
-    {
-        level: 9,
-        wood: 6050,
-        clay: 4840,
-        iron: 1815,
-        cereal: 3630
-    },
-    {
-        level: 10,
-        wood: 10105,
-        clay: 8080,
-        iron: 3030,
-        cereal: 6060
-    }
-];
+    {level: 1,wood: 100,clay: 80,iron: 30,cereal: 60},
+    {level: 2,wood: 165,clay: 135,iron: 50,cereal: 100},
+    {level: 3,wood: 280,clay: 225,iron: 85,cereal: 165},
+    {level: 4,wood: 465,clay: 375,iron: 140,cereal: 280},
+    {level: 5,wood: 780,clay: 620,iron: 235,cereal: 465},
+    {level: 6,wood: 1300,clay: 1040,iron: 390,cereal: 780},
+    {level: 7,wood: 2170,clay: 1735,iron: 650,cereal: 1300},
+    {level: 8,wood: 3625,clay: 2900,iron: 1085,cereal: 2175},
+    {level: 9,wood: 6050,clay: 4840,iron: 1815,cereal: 3630},
+    {level: 10,wood: 10105,clay: 8080,iron: 3030,cereal: 6060}];
 
 const cerealPrices = [
-    {
-        level: 1,
-        wood: 70,
-        clay: 90,
-        iron: 70,
-        cereal: 20
-    },
-    {
-        level: 2,
-        wood: 115,
-        clay: 150,
-        iron: 115,
-        cereal: 35
-    },
-    {
-        level: 3,
-        wood: 195,
-        clay: 250,
-        iron: 195,
-        cereal: 55
-    },
-    {
-        level: 4,
-        wood: 325,
-        clay: 420,
-        iron: 325,
-        cereal: 95
-    },
-    {
-        level: 5,
-        wood: 545,
-        clay: 700,
-        iron: 545,
-        cereal: 155
-    },
-    {
-        level: 6,
-        wood: 910,
-        clay: 1170,
-        iron: 910,
-        cereal: 260
-    },
-    {
-        level: 7,
-        wood: 1520,
-        clay: 1950,
-        iron: 1520,
-        cereal: 435
-    },
-    {
-        level: 8,
-        wood: 2535,
-        clay: 3260,
-        iron: 2535,
-        cereal: 725
-    },
-    {
-        level: 9,
-        wood: 4235,
-        clay: 5445,
-        iron: 4235,
-        cereal: 1210
-    },
-    {
-        level: 10,
-        wood: 7070,
-        clay: 9095,
-        iron: 7070,
-        cereal: 2020
-    }
-];
+    {level: 1,wood: 70,clay: 90,iron: 70,cereal: 20},
+    {level: 2,wood: 115,clay: 150,iron: 115,cereal: 35},
+    {level: 3,wood: 195,clay: 250,iron: 195,cereal: 55},
+    {level: 4,wood: 325,clay: 420,iron: 325,cereal: 95},
+    {level: 5,wood: 545,clay: 700,iron: 545,cereal: 155},
+    {level: 6,wood: 910,clay: 1170,iron: 910,cereal: 260},
+    {level: 7,wood: 1520,clay: 1950,iron: 1520,cereal: 435},
+    {level: 8,wood: 2535,clay: 3260,iron: 2535,cereal: 725},
+    {level: 9,wood: 4235,clay: 5445,iron: 4235,cereal: 1210},
+    {level: 10,wood: 7070,clay: 9095,iron: 7070,cereal: 2020}];
 
 const warehousePrices = [
-    {
-        level: 1,
-        size: 1200,
-        wood: 130,
-        clay: 160,
-        iron: 90,
-        cereal: 40
-    },
-    {
-        level: 2,
-        size: 1700,
-        wood: 165,
-        clay: 205,
-        iron: 115,
-        cereal: 50
-    },
-    {
-        level: 3,
-        size: 2300,
-        wood: 210,
-        clay: 260,
-        iron: 145,
-        cereal: 65
-    },
-    {
-        level: 4,
-        size: 3100,
-        wood: 275,
-        clay: 335,
-        iron: 190,
-        cereal: 85
-    },
-    {
-        level: 5,
-        size: 4000,
-        wood: 350,
-        clay: 430,
-        iron: 240,
-        cereal: 105
-    },
-    {
-        level: 6,
-        size: 5000,
-        wood: 445,
-        clay: 550,
-        iron: 310,
-        cereal: 135
-    },
-    {
-        level: 7,
-        size: 6300,
-        wood: 570,
-        clay: 705,
-        iron: 395,
-        cereal: 175
-    },
-    {
-        level: 8,
-        size: 7800,
-        wood: 730,
-        clay: 900,
-        iron: 505,
-        cereal: 225
-    },
-    {
-        level: 9,
-        size: 9600,
-        wood: 935,
-        clay: 1155,
-        iron: 650,
-        cereal: 290
-    },
-    {
-        level: 10,
-        size: 11800,
-        wood: 1200,
-        clay: 1475,
-        iron: 830,
-        cereal: 370
-    }
-];
+    {level: 1,size: 1200,wood: 130,clay: 160,iron: 90,cereal: 40},
+    {level: 2,size: 1700,wood: 165,clay: 205,iron: 115,cereal: 50},
+    {level: 3,size: 2300,wood: 210,clay: 260,iron: 145,cereal: 65},
+    {level: 4,size: 3100,wood: 275,clay: 335,iron: 190,cereal: 85},
+    {level: 5,size: 4000,wood: 350,clay: 430,iron: 240,cereal: 105},
+    {level: 6,size: 5000,wood: 445,clay: 550,iron: 310,cereal: 135},
+    {level: 7,size: 6300,wood: 570,clay: 705,iron: 395,cereal: 175},
+    {level: 8,size: 7800,wood: 730,clay: 900,iron: 505,cereal: 225},
+    {level: 9,size: 9600,wood: 935,clay: 1155,iron: 650,cereal: 290},
+    {level: 10,size: 11800,wood: 1200,clay: 1475,iron: 830,cereal: 370}];
 
 const barnPrices = [
-    {
-        level: 1,
-        size: 1200,
-        wood: 80,
-        clay: 100,
-        iron: 70,
-        cereal: 20
-    },
-    {
-        level: 2,
-        size: 1700,
-        wood: 100,
-        clay: 130,
-        iron: 90,
-        cereal: 25
-    },
-    {
-        level: 3,
-        size: 2300,
-        wood: 130,
-        clay: 165,
-        iron: 115,
-        cereal: 35
-    },
-    {
-        level: 4,
-        size: 3100,
-        wood: 170,
-        clay: 210,
-        iron: 145,
-        cereal: 40
-    },
-    {
-        level: 5,
-        size: 4000,
-        wood: 215,
-        clay: 270,
-        iron: 190,
-        cereal: 55
-    },
-    {
-        level: 6,
-        size: 5000,
-        wood: 275,
-        clay: 345,
-        iron: 240,
-        cereal: 70
-    },
-    {
-        level: 7,
-        size: 6300,
-        wood: 350,
-        clay: 440,
-        iron: 310,
-        cereal: 90
-    },
-    {
-        level: 8,
-        size: 7800,
-        wood: 450,
-        clay: 565,
-        iron: 395,
-        cereal: 115
-    },
-    {
-        level: 9,
-        size: 9600,
-        wood: 575,
-        clay: 720,
-        iron: 505,
-        cereal: 145
-    },
-    {
-        level: 10,
-        size: 11800,
-        wood: 740,
-        clay: 920,
-        iron: 645,
-        cereal: 185
-    }
-];
+    {level: 1,size: 1200,wood: 80,clay: 100,iron: 70,cereal: 20},
+    {level: 2,size: 1700,wood: 100,clay: 130,iron: 90,cereal: 25},
+    {level: 3,size: 2300,wood: 130,clay: 165,iron: 115,cereal: 35},
+    {level: 4,size: 3100,wood: 170,clay: 210,iron: 145,cereal: 40},
+    {level: 5,size: 4000,wood: 215,clay: 270,iron: 190,cereal: 55},
+    {level: 6,size: 5000,wood: 275,clay: 345,iron: 240,cereal: 70},
+    {level: 7,size: 6300,wood: 350,clay: 440,iron: 310,cereal: 90},
+    {level: 8,size: 7800,wood: 450,clay: 565,iron: 395,cereal: 115},
+    {level: 9,size: 9600,wood: 575,clay: 720,iron: 505,cereal: 145},
+    {level: 10,size: 11800,wood: 740,clay: 920,iron: 645,cereal: 185}];
 
 // Production increases
 const productionIncreases = [7, 13, 21, 31, 46, 70, 98, 140, 203, 280];
@@ -511,10 +128,10 @@ const cerealButton3 = document.getElementById("cereal-button-3");
 const cerealButton4 = document.getElementById("cereal-button-4");
 const cerealButton5 = document.getElementById("cereal-button-5");
 
-const woodHourText = document.getElementById("wood-hour-Text");
-const clayHourText = document.getElementById("clay-hour-Text");
-const ironHourText = document.getElementById("iron-hour-Text");
-const cerealHourText = document.getElementById("cereal-hour-Text");
+const woodHourText = document.getElementById("wood-hour-text");
+const clayHourText = document.getElementById("clay-hour-text");
+const ironHourText = document.getElementById("iron-hour-text");
+const cerealHourText = document.getElementById("cereal-hour-text");
 
 const warehouseButton = document.getElementById("warehouse-button");
 const barnButton = document.getElementById("barn-button");
@@ -526,7 +143,6 @@ const produceButton = document.getElementById("production-button");
 
 let currentwarehouseIndex = 0;
 let currentBarnIndex = 0;
-
 
 // Function
 function produce(){
@@ -595,11 +211,11 @@ function warehouseUpgrade(){
 }
 warehouseButton.onclick = warehouseUpgrade;
 
-
 // WOOD Handling
 function upgradeWood(index){
     if (woodFields[index] == 10){
-        // NO SE PUEDE, MAXIMO
+        information.innerText = "Leñador a nivel máximo, no se puede ampliar más.";
+        information.style.display = "flex";
     }
     else{
         // Cambiar boton
@@ -609,18 +225,52 @@ function upgradeWood(index){
         let i = woodPrices[woodFields[index]].iron;
         let ce = woodPrices[woodFields[index]].cereal;
         if (priceCheck(w, c, i, ce)){
-            // Si me alcanza, comprar, aumentar la produccion y actualizar textos
+            
+            // Confirmation text
+            information.innerText = "Aumentaste un leñador";
+            information.style.display = "flex";
+            
+            // Update resources and production
+            wood -= w;
+            clay -= c;
+            iron -= i;
+            cereal -= ce;
+            woodHour += productionIncreases[woodFields[index]];
+
+            // Update field information
+            woodFields[index]++;
+
+            // Update texts
+            woodText.innerText = wood;
+            clayText.innerText = clay;
+            ironText.innerText = iron;
+            cerealText.innerText = cereal;
+            woodHourText.innerText = woodHour;
         }
         else{
             // NO SE PUEDE, NO HAY MONEY
+            information.innerText = "No tienes los suficientes recursos";
+            information.style.display = "flex";
         }
     }
 }
 
-function upgradeWood1(){upgradeWood(0);}
-function upgradeWood2(){upgradeWood(1);}
-function upgradeWood3(){upgradeWood(2);}
-function upgradeWood4(){upgradeWood(3);}
+function upgradeWood1(){
+    upgradeWood(0);
+    woodButton1.innerText = woodFields[0];
+}
+function upgradeWood2(){
+    upgradeWood(1);
+    woodButton2.innerText = woodFields[1];
+}
+function upgradeWood3(){
+    upgradeWood(2);
+    woodButton3.innerText = woodFields[2];
+}
+function upgradeWood4(){
+    upgradeWood(3);
+    woodButton4.innerText = woodFields[3];
+}
 
 woodButton1.onclick = upgradeWood1;
 woodButton2.onclick = upgradeWood2;
---
+++ try.js
@@ -0,0 +1,59 @@
+const resourcesPrices = [
+    [{level: 1, wood: 40, clay: 100, iron: 50, cereal: 60},
+    {level: 2, wood: 65, clay: 165, iron: 85, cereal: 100},
+    {level: 3, wood: 110, clay: 280, iron: 140, cereal: 165},
+    {level: 4, wood: 185, clay: 465, iron: 235, cereal: 280},
+    {level: 5, wood: 310, clay: 780, iron: 390, cereal: 465},
+    {level: 6, wood: 520, clay: 1300, iron: 650, cereal: 780},
+    {level: 7, wood: 870, clay: 2170, iron: 1085, cereal: 1300},
+    {level: 8, wood: 1450, clay: 3625, iron: 1810, cereal: 2175},
+    {level: 9, wood: 2420, clay: 6050, iron: 3025, cereal: 3630},
+    {level: 10, wood: 4040, clay: 10105, iron: 5050, cereal: 6060}],
+
+    [{level: 1,wood: 80,clay: 40,iron: 80,cereal: 50},
+    {level: 2,wood: 135,clay: 65,iron: 135,cereal: 85},
+    {level: 3,wood: 225,clay: 110,iron: 225,cereal: 140},
+    {level: 4,wood: 375,clay: 185,iron: 375,cereal: 235},
+    {level: 5,wood: 620,clay: 310,iron: 620,cereal: 390},
+    {level: 6,wood: 1040,clay: 520,iron: 1040,cereal: 650},
+    {level: 7,wood: 1735,clay: 870,iron: 1735,cereal: 1085},
+    {level: 8,wood: 2900,clay: 1450,iron: 2900,cereal: 1810},
+    {level: 9,wood: 4840,clay: 2420,iron: 4840,cereal: 3025},
+    {level: 10,wood: 8080,clay: 4040,iron: 8080,cereal: 5050}],
+
+    [{level: 1,wood: 100,clay: 80,iron: 30,cereal: 60},
+    {level: 2,wood: 165,clay: 135,iron: 50,cereal: 100},
+    {level: 3,wood: 280,clay: 225,iron: 85,cereal: 165},
+    {level: 4,wood: 465,clay: 375,iron: 140,cereal: 280},
+    {level: 5,wood: 780,clay: 620,iron: 235,cereal: 465},
+    {level: 6,wood: 1300,clay: 1040,iron: 390,cereal: 780},
+    {level: 7,wood: 2170,clay: 1735,iron: 650,cereal: 1300},
+    {level: 8,wood: 3625,clay: 2900,iron: 1085,cereal: 2175},
+    {level: 9,wood: 6050,clay: 4840,iron: 1815,cereal: 3630},
+    {level: 10,wood: 10105,clay: 8080,iron: 3030,cereal: 6060}],
+
+    [{level: 1,wood: 70,clay: 90,iron: 70,cereal: 20},
+    {level: 2,wood: 115,clay: 150,iron: 115,cereal: 35},
+    {level: 3,wood: 195,clay: 250,iron: 195,cereal: 55},
+    {level: 4,wood: 325,clay: 420,iron: 325,cereal: 95},
+    {level: 5,wood: 545,clay: 700,iron: 545,cereal: 155},
+    {level: 6,wood: 910,clay: 1170,iron: 910,cereal: 260},
+    {level: 7,wood: 1520,clay: 1950,iron: 1520,cereal: 435},
+    {level: 8,wood: 2535,clay: 3260,iron: 2535,cereal: 725},
+    {level: 9,wood: 4235,clay: 5445,iron: 4235,cereal: 1210},
+    {level: 10,wood: 7070,clay: 9095,iron: 7070,cereal: 2020}]
+];
+
+function upgradeField(resource, index){
+
+}
+
+function upgradeWood(){}
+
+function upgradeClay(){}
+
+function upgradeIron(){}
+
+function upgradeCereal(){
+    
+}
